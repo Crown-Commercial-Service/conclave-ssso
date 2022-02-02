ef50b212b44eba6413254ebda4321ac4af0e66d8
Tharindu Subashith <tharindu.subashith@brickendon.com>
2022-02-02 18:49:17 +0530

    Add partial scripts

---
+++ api/CcsSso.Core.DbMigrations/DevScripts/RoleBackScripts/RoleBackFleetPortalServiceRoles.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION drop_flp_service() RETURNS integer AS $$
+
+-- Add values to clientId and clientUrl
+DECLARE serviceName text = 'Fleet Portal';
+DECLARE dashboardAccessRoleKey text = 'ACCESS_FP_CLIENT';
+DECLARE serviceRoleKey1 text = 'FP_USER';
+
+BEGIN
+
+	RETURN 1;
+	END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT drop_flp_service();
+DROP FUNCTION drop_flp_service;
+SELECT setval('"CcsAccessRole_Id_seq"', max("Id")) FROM "CcsAccessRole";
+SELECT setval('"CcsService_Id_seq"', max("Id")) FROM "CcsService";
+SELECT setval('"ServicePermission_Id_seq"', max("Id")) FROM "ServicePermission";
+SELECT setval('"ServiceRolePermission_Id_seq"', max("Id")) FROM "ServiceRolePermission";
