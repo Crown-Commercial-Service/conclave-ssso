a5dff9daa32a592603ec65f9c2f463fbcced5f52
KarpagamNallasamy-bc <97511557+karpagamNallasamy@users.noreply.github.com>
2022-03-23 09:39:37 +0530

    allow to add user without select title (#453)
    * allow to add user without select title
    
    * apply null checking with empty space
    
    * change string data type.

--- api/CcsSso.Core.Service/External/UserProfileService.cs
+++ api/CcsSso.Core.Service/External/UserProfileService.cs
@@ -72,7 +72,6 @@ namespace CcsSso.Core.Service.External
       {
         throw new ResourceAlreadyExistsException();
       }
-
       Validate(userProfileRequestInfo, false, organisation);
 
       var eligibleIdentityProviders = await _dataContext.OrganisationEligibleIdentityProvider
@@ -153,7 +152,7 @@ namespace CcsSso.Core.Service.External
         User = new User
         {
           UserName = userName,
-          UserTitle = (int)Enum.Parse(typeof(UserTitle), userProfileRequestInfo.Title),
+          UserTitle = (int)Enum.Parse(typeof(UserTitle), string.IsNullOrWhiteSpace(userProfileRequestInfo.Title) ? "Unspecified" : userProfileRequestInfo.Title),
           UserGroupMemberships = userGroupMemberships,
           UserAccessRoles = userAccessRoles,
           UserIdentityProviders = userProfileRequestInfo.Detail.IdentityProviderIds.Select(idpId => new UserIdentityProvider
@@ -507,7 +506,7 @@ namespace CcsSso.Core.Service.External
       bool mfaFlagChanged = user.MfaEnabled != userProfileRequestInfo.MfaEnabled;
       bool hasProfileInfoChanged = (user.Party.Person.FirstName != userProfileRequestInfo.FirstName.Trim() ||
                                     user.Party.Person.LastName != userProfileRequestInfo.LastName.Trim() ||
-                                    user.UserTitle != (int)Enum.Parse(typeof(UserTitle), userProfileRequestInfo.Title) ||
+                                    user.UserTitle != (int)Enum.Parse(typeof(UserTitle), string.IsNullOrWhiteSpace(userProfileRequestInfo.Title) ? "Unspecified" : userProfileRequestInfo.Title) ||
                                     user.UserIdentityProviders.Select(uidp => uidp.OrganisationEligibleIdentityProviderId).OrderBy(id => id) != userProfileRequestInfo.Detail.IdentityProviderIds.OrderBy(id => id));
 
       user.Party.Person.FirstName = userProfileRequestInfo.FirstName.Trim();
