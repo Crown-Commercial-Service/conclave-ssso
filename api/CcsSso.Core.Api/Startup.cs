96474a9668ca80f9b15ea846d69055c37d789b66
tsbrickendon <84302916+tsbrickendon@users.noreply.github.com>
2022-03-23 09:59:56 +0530

    Additional logs (#456)
    * Initial logs
    
    * Add response logs
    
    * Improve logs
    
    * Add config for switch

--- api/CcsSso.Core.Api/Startup.cs
+++ api/CcsSso.Core.Api/Startup.cs
@@ -143,7 +143,7 @@ namespace CcsSso.Api
           DefaultTypeValidationValue = Configuration["DocUpload:TypeValidationValue"],
         };
         return docUploadConfig;
-      }); 
+      });
 
       services.AddSingleton(s =>
       {
@@ -226,9 +226,9 @@ namespace CcsSso.Api
       services.AddScoped<IUserProfileHelperService, UserProfileHelperService>();
       services.AddScoped<IIdamService, IdamService>();
       services.AddScoped<IConfigurationDetailService, ConfigurationDetailService>();
-      services.AddScoped<IDocUploadService, DocUploadService>(); 
-      services.AddScoped<IBulkUploadService, BulkUploadService>(); 
-      services.AddScoped<IBulkUploadFileContentService, BulkUploadFileContentService>(); 
+      services.AddScoped<IDocUploadService, DocUploadService>();
+      services.AddScoped<IBulkUploadService, BulkUploadService>();
+      services.AddScoped<IBulkUploadFileContentService, BulkUploadFileContentService>();
 
       services.AddHttpContextAccessor();
 
@@ -263,15 +263,27 @@ namespace CcsSso.Api
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IAntiforgery antiforgery)
     {
-      app.UseMiddleware<CommonExceptionHandlerMiddleware>();
-      app.UseSwagger();
-      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CcsSso.Api v1"));
+
       app.UseHsts();
       app.UseHttpsRedirection();
 
       app.Use(async (context, next) =>
       {
         context.Request.EnableBuffering();
+        await next();
+      });
+
+      bool additionalLog = Configuration.GetSection("EnableAdditionalLogs").Get<bool>();
+      if (additionalLog)
+      {
+        app.UseMiddleware<RequestLogMiddleware>();
+      }
+      app.UseMiddleware<CommonExceptionHandlerMiddleware>();
+      app.UseSwagger();
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CcsSso.Api v1"));
+
+      app.Use(async (context, next) =>
+      {
         var customDomain = Configuration.GetSection("CustomDomain").Get<string>();
         context.Response.Headers.Add(
             "Cache-Control",
---
+++ api/CcsSso.Security.Api/Middleware/RequestLogMiddleware.cs
@@ -0,0 +1,71 @@
+﻿using CcsSso.Security.Domain.Dtos;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CcsSso.Security.Api.Middleware
+{
+  public class RequestLogMiddleware
+  {
+    private RequestDelegate _next;
+    private readonly ApplicationConfigurationInfo _applicationConfigurationInfo;
+
+    public RequestLogMiddleware(RequestDelegate next, ApplicationConfigurationInfo applicationConfigurationInfo)
+    {
+      _next = next;
+      _applicationConfigurationInfo = applicationConfigurationInfo;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+      var path = context.Request.Path.Value.TrimStart('/').TrimEnd('/');
+      if (path != "swagger/index.html" && path != "swagger/v1/swagger.json")
+      {
+        var fullPath = context.Request.Path.Value;
+        var queryString = context.Request.QueryString;
+        var method = context.Request.Method;
+        var apiKey = context.Request.Headers["X-API-Key"].FirstOrDefault();
+        var bearerToken = context.Request.Headers["Authorization"].FirstOrDefault();
+        var token = bearerToken?.Split(' ').Last();
+        string requestBodyString;
+        string responseBodyString;
+
+        using (var reader = new StreamReader(context.Request.Body, encoding: Encoding.UTF8, detectEncodingFromByteOrderMarks: false, leaveOpen: true))
+        {
+          requestBodyString = await reader.ReadToEndAsync();
+          // Reset the request body stream position so the next middleware can read it
+          context.Request.Body.Position = 0;
+        }
+
+        var originalBody = context.Response.Body;
+        using var newBody = new MemoryStream();
+        context.Response.Body = newBody;
+
+        try
+        {
+          await _next(context);
+        }
+        finally
+        {
+          newBody.Seek(0, SeekOrigin.Begin);
+          using (var bodyTextReader = new StreamReader(context.Response.Body))
+          {
+            responseBodyString = await bodyTextReader.ReadToEndAsync();
+            newBody.Seek(0, SeekOrigin.Begin);
+            await newBody.CopyToAsync(originalBody);
+          }
+
+          Console.WriteLine($"SECURITY-API-LOGS:- Method: {method}, Path: {fullPath}, Query: {queryString}, Token: {token}, ApiKey: {apiKey}, RequestBody: {JsonConvert.SerializeObject(requestBodyString)}, Status: {context.Response.StatusCode}, ResponseBody: {responseBodyString}");
+        }
+      }
+      else
+      {
+        await _next(context);
+      }
+    }
+  }
+}
