5f570fb0fa5cdd7e53c62885dfa67ac2e515cfe3
KarpagamNallasamy-bc <97511557+karpagamNallasamy@users.noreply.github.com>
2022-04-12 18:15:49 +0530

    bug fix in tile description (#505)

--- api/CcsSso.Core.DbMigrations/Scripts/Phase2_Sprint5/202204111702_CATAccessKeyRole.sql
+++ api/CcsSso.Core.DbMigrations/Scripts/Phase2_Sprint5/202204111702_CATAccessKeyRole.sql
@@ -1,17 +1,21 @@
 START TRANSACTION;
 
 UPDATE public."CcsAccessRole" 
-SET "CcsAccessRoleNameKey" ='ACCESS_CAAAC_CLIENT',
-"CcsAccessRoleName"='Access Contract Award Service',
+SET "CcsAccessRoleName"='Access Contract Award Service',
+"CcsAccessRoleDescription"='Access Contract Award Service',
 "LastUpdatedOnUtc"=Now()
-WHERE "CcsAccessRoleNameKey"='ACCESS_CAT_CLIENT';
+WHERE "CcsAccessRoleNameKey"='ACCESS_CAAAC_CLIENT';
 
 UPDATE public."CcsAccessRole" 
-SET "CcsAccessRoleNameKey" ='CAT_USER',
-"CcsAccessRoleName"='CAS User',
+SET "CcsAccessRoleName"='CAS User',
 "CcsAccessRoleDescription"='CAS User',
 "LastUpdatedOnUtc"=Now()
 WHERE "CcsAccessRoleNameKey"='CAT_USER';
 
-COMMIT;
+UPDATE public."CcsAccessRole" 
+SET "CcsAccessRoleName"='CAS User',
+"CcsAccessRoleDescription"='CAS User',
+"LastUpdatedOnUtc"=Now()
+WHERE "CcsAccessRoleNameKey"='CAT_USER_LOGIN_DIRECTOR';
 
+COMMIT;
