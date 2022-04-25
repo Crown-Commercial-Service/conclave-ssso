3e476492c3be72949342468a726f3179ccd55e07
tsbrickendon <84302916+tsbrickendon@users.noreply.github.com>
2022-04-25 17:58:35 +0530

    Add site contact point deletion check for user deletion job (#536)
    * Add site contact point deletion check for user deletion job
    
    * Add assigned contact check

--- api/CcsSso.Core.Service/ContactSupportService.cs
+++ api/CcsSso.Core.Service/ContactSupportService.cs
@@ -35,7 +35,9 @@ namespace CcsSso.Core.Service
       }
       else
       {
-        var siteContactsAvailable = await _dataContext.SiteContact.AnyAsync(sc => !sc.IsDeleted && sc.ContactPoint.PartyId == organisation.PartyId
+        var siteContactsAvailable = await _dataContext.SiteContact.AnyAsync(sc => !sc.IsDeleted && !sc.ContactPoint.IsDeleted
+        && sc.OriginalContactId == 0 //not assigned contacts
+        && sc.ContactPoint.PartyId == organisation.PartyId
         && !userContacPointIds.Contains(sc.OriginalContactId));
         Console.WriteLine($"SiteContactsAvailable: {siteContactsAvailable}");
         return siteContactsAvailable;
