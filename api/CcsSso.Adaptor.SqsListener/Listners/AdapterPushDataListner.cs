d4b8c7395f981823923461c429207d8dec3adff8
tharindu <tharindu.subashith@brickendon.com>
2022-04-27 06:47:51 +0530

    Improve logs on adaptor push

--- api/CcsSso.Adaptor.SqsListener/Listners/AdapterPushDataListner.cs
+++ api/CcsSso.Adaptor.SqsListener/Listners/AdapterPushDataListner.cs
@@ -3,6 +3,7 @@ using CcsSso.Shared.Contracts;
 using CcsSso.Shared.Domain.Dto;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -68,17 +69,17 @@ namespace CcsSso.Adaptor.SqsListener.Listners
 
         if (response.IsSuccessStatusCode)
         {
-          Console.WriteLine($"Worker: {LISTNER_JOB_NAME} :: Message processing succeeded at: {DateTime.UtcNow}");
+          Console.WriteLine($"WorkerScuccess: {LISTNER_JOB_NAME} :: Message processing succeeded for url: {url}, data: {JsonConvert.SerializeObject(sqsMessageResponseDto.MessageBody)}, at: {DateTime.UtcNow}");
           await DeleteMessageFromQueueAsync(sqsMessageResponseDto);
         }
         else
         {
           var responseContent = await response.Content.ReadAsStringAsync();
-          _logger.LogError($"Worker: {LISTNER_JOB_NAME} :: Message processing error at: {DateTime.UtcNow}");
+          Console.WriteLine($"WorkerError: {LISTNER_JOB_NAME} :: Message processing error for MessageId: {sqsMessageResponseDto.MessageId}, url: {url}, data: {JsonConvert.SerializeObject(sqsMessageResponseDto.MessageBody)}, at: {DateTime.UtcNow}, ErroreCode: {response.StatusCode}, Error: {JsonConvert.SerializeObject(responseContent)}");
           _logger.LogError($"Worker: {LISTNER_JOB_NAME} :: MessageId: {sqsMessageResponseDto.MessageId}, ErroreCode: {response.StatusCode}, Error: {responseContent}");
           if (sqsMessageResponseDto.ReceiveCount > _appSetting.SqsListnerJobSetting.MessageReadThreshold)
           {
-            Console.WriteLine($"Worker: {LISTNER_JOB_NAME} :: MessageId {sqsMessageResponseDto.MessageId} receive count exceeded at {DateTime.UtcNow}");
+            Console.WriteLine($"Worker: {LISTNER_JOB_NAME} :: MessageId {sqsMessageResponseDto.MessageId} receive count exceeded at {DateTime.UtcNow} for, url: {url}");
             // TODO delete and send to deadletter queue
             await DeleteMessageFromQueueAsync(sqsMessageResponseDto);
           }
