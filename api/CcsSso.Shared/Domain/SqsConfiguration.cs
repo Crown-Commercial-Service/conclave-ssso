7d76f146a0ef7772a799ba57c2b65017a9bb0be7
ponselvamsakthivel-bc <87376328+ponselvamsakthivel-bc@users.noreply.github.com>
2022-06-23 03:37:39 +0100

    p2sprin10 release for adaptor (#688)
    * Remove attribute is admin user task1189 p2sprint10 (#676)
    
    * remove attribute IsAdminuser
    
    * remove 'isAdminUser' determine inside method
    
    * remove unwanted copy function
    
    * Con 2407 con 2543 jaggaertile p2sprint10 (#677)
    
    * Update Tile and Description for tile - JAGGAER
    
    * remove unwanted script file
    
    * PR-3092-Feature/3092 backchannel logout hotfix 20 06 2022 (#672)
    
    * BC- Hot Fix - client first login user journey
    
    * sprint 9-Hot fix BC logout reverse the cookie or token check
    
    * Hot fix for back channel logout
    
    * All BC related debug lines are removed
    
    * Con 2367 input fields validation for api is p2sprint10 (#684)
    
    * Input fields validation for APIs Group Name
    
    * Input fields validation for APIs ContactPointName
    
    * Street address related validations
    
    * sitename API call validation
    
    * API Input validation should allow space
    
    * remove unwanted comment lines
    
    * apply proper text based on validation type.
    
    * Separate SQS with id and secrets - New app settings added and relevant changes are done in the classes and interfaces (#685)
    
    Co-authored-by: KarpagamNallasamy-bc <97511557+karpagamNallasamy@users.noreply.github.com>
    Co-authored-by: VijayHirudayasamy-bc <103512567+VijayHirudayasamy-bc@users.noreply.github.com>

--- api/CcsSso.Shared/Domain/SqsConfiguration.cs
+++ api/CcsSso.Shared/Domain/SqsConfiguration.cs
@@ -12,6 +12,11 @@ namespace CcsSso.Shared.Domain
 
     public string AccessSecretKey { get; set; }
 
+    public string AdaptorNotificationAccessKeyId { get; set; }
+    public string AdaptorNotificationAccessSecretKey { get; set; }
+    public string PushDataAccessKeyId { get; set; }
+    public string PushDataAccessSecretKey { get; set; }
+
     public int RecieveMessagesMaxCount { get; set; }
 
     public int RecieveWaitTimeInSeconds { get; set; }
