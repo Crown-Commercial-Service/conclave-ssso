8dc17debd0207560666785ee32f44b4a5851352f
karpagamnallasamy-bc <karpagam.nallasamy@brickendon.com>
2022-07-01 22:05:49 +0530

    revert the code related bugfix 2363

--- api/CcsSso.Core.ExternalApi/Middleware/InputValidationMiddleware.cs
+++ api/CcsSso.Core.ExternalApi/Middleware/InputValidationMiddleware.cs
@@ -20,31 +20,15 @@ namespace CcsSso.Core.ExternalApi.Middleware
 
     public async Task Invoke(HttpContext context)
     {
-      string Method = context.Request.Method;
-      string Path = context.Request.Path;
-      string[] pathList = Path.Split("/");
-      
       using (var reader = new StreamReader(context.Request.Body, encoding: Encoding.UTF8, detectEncodingFromByteOrderMarks: false, leaveOpen: true))
       {
         var bodyString = await reader.ReadToEndAsync();
 
         // Reset the request body stream position so the next middleware can read it
         context.Request.Body.Position = 0;
-
-        if (Method == "POST" || Method == "PUT")
+        if (bodyString.IsInvalidCharactorIncluded(RegexExpression.INVALID_CHARACTORS_FOR_API_INPUT))
         {
-          if ((pathList[1] == "organisations" || pathList[1] == "organisation-profiles") && (pathList[3] == "groups" || pathList[3] == "sites" || pathList[3] == "contacts"))
-          {
-            await _next(context);
-            return;
-          }
-        }
-        else
-        {
-          if (bodyString.IsInvalidCharactorIncluded(RegexExpression.INVALID_CHARACTORS_FOR_API_INPUT))
-          {
-            throw new CcsSsoException("ERROR_INVALID_INPUT_CHARACTER");
-          }
+          throw new CcsSsoException("ERROR_INVALID_INPUT_CHARACTER");
         }
       }
       await _next(context);
