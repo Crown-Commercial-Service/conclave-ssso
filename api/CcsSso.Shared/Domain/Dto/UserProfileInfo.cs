023d60b7cbc46d12bc7d9450769a426279e74530
ponselvamsakthivel-bc <87376328+ponselvamsakthivel-bc@users.noreply.github.com>
2022-08-10 22:35:41 +0530

    P2sprint11 merge with main branch (#807)
    * Upgrade .net core version to six (#739)
    
    * CON-2391 - Reporting requirement - change detection - User-Priority 1 (#734)
    
    * Initial Code Set Up
    
    * Enabled Organization Reporting
    
    * Optimization - Removed Testing code
    
    * Typo- Corrected
    
    * Used Method Overload for CSV
    
    * Feature/3341 con 2391 reporting requirement framework creation improvements sprint 11 (#745)
    
    * Reporting framework improvement has been done
    
    * Applied changes to the user reporting job
    
    * CON-2391 - Reporting requirement - change deduction - Audit-priority 3 (#740)
    
    * Initial Code Set Up for Audit Log Report
    
    * Audit Log Reporting Job Implemented and Fixed the Typos in the UserReporting Job.
    
    * Implements the Reporting Logging part in Users and Audit Reporting Job
    
    * File Name Correction in Org , User, Audit
    
    * Event Time Changes condition (#746)
    
    * 2116 & 2114 - Fix manage service eligibility access issue (#749)
    
    * Feature/2620 federated login upto AC4 (#753)
    
    * CON-2620 -AC1 to AC4 Done
    AC5 Halfway through.
    
    * some fixes are done for the ACs upto 4. Some of the AC5 work
    
    * missing interface (#754)
    
    * feature/3188-CON-2391-Reporting-CD-Contact for PR (#758)
    
    * Initial Code  - Org Contact Implemented
    
    * CSV Header Formatted for User, Org report and also aligned the data.
    
    * Implemented the User, Site Contact functionality
    
    * Formatting and Aligned changes are implemented
    
    * Buddy Check Changes are implemented
    
    * Feature/dv check 3188 con 2391 reporting cd contact (#760)
    
    * Analysing the BUG- 3453
    
    * Bug 3453  - Fix and Code Changes for loggers
    
    * Changes has been made for removing (#761)
    
    additional secuirty validation while
    adding the user with social login selected.
    And removed forced login when sign-in providers are
    removed
    
    * Ambiguity Issue in Dev for Jobs are fixed. (#767)
    
    * Reverted manage service eligibility fix api (#768)
    
    * 2116 & 2114 - Fix manage service eligibility access issue
    
    * Revert fix of 1373 & 1453
    
    * fix - upgrade Azure.Storage.Blobs package (#771)
    
    * Feature/3457 framework ppg data extracts issues by stuart for PR (#774)
    
    * Changes in the filename with case and header name modified
    
    * CSV file Space Issue
    
    * Typo fix of the contact File name
    
    * BE-2620- AC point after 5. sending email based on selected IDPs when create users in the organistion (#775)
    
    * CON-2620-AC5 email template changes are completed. (#776)
    
    * Missign files are added (#778)
    
    * bug 3479 has been fixed. (#779)
    
    * Feature/bugs Audit , Users, Contact (For existing site) for PR (#783)
    
    * Initial - bug fix code changes
    
    * Bug Fixes for Audit , Site Contact for the existing site
    
    * Changes on the Contact Report for exisitng Site
    
    * Bug fixes for User and existing contact reports
    
    * New email template for userid/password sign-in provider when it is added from the manage organisation (#785)
    
    and space between sign-in provider in the email.
    
    * Userid/pwd has been removed from the sign-in provider list (#790)
    
    * Audit report to write direct api call when the user id is zero (#789)
    
    * User reporting bugs are fixed (#792)
    
    * issue fixed- reset variables inside the forloop (#795)
    
    * feature/BC-logout-missed-file-added-in-p2sprint11-pons (#804)
    
    Co-authored-by: brijeshpatel-bc <108462846+brijeshpatel-bc@users.noreply.github.com>
    Co-authored-by: SureshKumarD-bc <108136042+SureshKumarD-bc@users.noreply.github.com>
    Co-authored-by: VijayHirudayasamy-bc <103512567+VijayHirudayasamy-bc@users.noreply.github.com>

---
+++ api/CcsSso.Shared/Domain/Dto/UserProfileInfo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CcsSso.Shared.Domain.Dto
+{
+     
+    public class UserProfileResponseInfo
+    {
+        public UserDetails detail { get; set; }
+        public string UserName { get; set; }
+        public string OrganisationId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public bool mfaEnabled { get; set; }
+        public string Password { get; set; }
+        public bool AccountVerified { get; set; }
+        public bool SendUserRegistrationEmail { get; set; }
+        public bool IsAdminUser { get; set; }
+
+    }
+
+    public class UserDetails
+    {
+        public int Id { get; set; }
+        public List<GroupAccessRole> userGroups { get; set; }
+        public bool CanChangePassword { get; set; }
+        
+        public List<RolePermissionInfo> rolePermissionInfo { get; set; }
+        public List<IdentityProviders> identityProviders { get; set; }
+        
+    }
+
+    public class GroupAccessRole
+    {
+        public int GroupId { get; set; }
+
+        public string AccessRole { get; set; }
+
+        public string AccessRoleName { get; set; }
+
+        public string Group { get; set; }
+
+        public string ServiceClientName { get; set; }
+    }
+
+    public class RolePermissionInfo
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleKey { get; set; }
+        public string ServiceClientId { get; set; }
+        public string ServiceClientName { get; set; }
+    }
+        
+    public class IdentityProviders
+    {
+        public int IdentityProviderId { get; set; }
+        public string IdentityProvider { get; set; }
+        public string IdentityProviderDisplayName { get; set; }
+
+    }
+}
