02ddc2b818f1575a83f7943a709f25a5e5b78d4f
chudasama brijrajsinh <chudasama.brijrajsinh@brickendon.com>
2022-08-16 15:21:35 +0530

    Delegated users list added role list

--- api/CcsSso.Core.Domain/Dtos/External/UserProfileResponseInfo.cs
+++ api/CcsSso.Core.Domain/Dtos/External/UserProfileResponseInfo.cs
@@ -9,177 +9,179 @@ using System.ComponentModel.DataAnnotations;
 namespace CcsSso.Core.Domain.Dtos.External
 {
 
-    public class UserDetail
-    {
-        public string UserName { get; set; }
+  public class UserDetail
+  {
+    public string UserName { get; set; }
 
-        public string OrganisationId { get; set; }
+    public string OrganisationId { get; set; }
 
-        public string FirstName { get; set; }
+    public string FirstName { get; set; }
 
-        public string LastName { get; set; }
+    public string LastName { get; set; }
 
-        public string Title { get; set; }
+    public string Title { get; set; }
 
-        public bool MfaEnabled { get; set; }
+    public bool MfaEnabled { get; set; }
 
-        public string Password { get; set; }
+    public string Password { get; set; }
 
-        public bool AccountVerified { get; set; }
+    public bool AccountVerified { get; set; }
 
-        public bool SendUserRegistrationEmail { get; set; } = true;
+    public bool SendUserRegistrationEmail { get; set; } = true;
 
-        public string? OriginOrganisationName { get; set; }
+    public string? OriginOrganisationName { get; set; }
 
-    }
+  }
 
-    public class UserRequestDetail
-    {
-        public int Id { get; set; }
+  public class UserRequestDetail
+  {
+    public int Id { get; set; }
 
-        public List<int> GroupIds { get; set; }
+    public List<int> GroupIds { get; set; }
 
-        public List<int> RoleIds { get; set; }
+    public List<int> RoleIds { get; set; }
 
-        public List<int> IdentityProviderIds { get; set; }
-    }
+    public List<int> IdentityProviderIds { get; set; }
+  }
 
-    public class UserResponseDetail
-    {
-        public int Id { get; set; }
+  public class UserResponseDetail
+  {
+    public int Id { get; set; }
 
-        public List<GroupAccessRole> UserGroups { get; set; }
+    public List<GroupAccessRole> UserGroups { get; set; }
 
-        public bool CanChangePassword { get; set; }
+    public bool CanChangePassword { get; set; }
 
-        public List<RolePermissionInfo> RolePermissionInfo { get; set; }
+    public List<RolePermissionInfo> RolePermissionInfo { get; set; }
 
-        public List<UserIdentityProviderInfo> IdentityProviders { get; set; }
+    public List<UserIdentityProviderInfo> IdentityProviders { get; set; }
 
-        public UserDelegationDetails[]? DelegatedOrgs { get; set; }
-    }
+    public UserDelegationDetails[]? DelegatedOrgs { get; set; }
+  }
 
-    public class UserDelegationDetails
-    {
-        public string? DelegatedOrgId { get; set; }
+  public class UserDelegationDetails
+  {
+    public string? DelegatedOrgId { get; set; }
 
-        public string? DelegatedOrgName { get; set; }
+    public string? DelegatedOrgName { get; set; }
 
-        public DateTime? StartDate { get; set; }
+    public DateTime? StartDate { get; set; }
 
-        public DateTime? EndDate { get; set; }
-        
-        public bool? DelegationAccepted { get; set; }
-    }
+    public DateTime? EndDate { get; set; }
 
-    public class UserIdentityProviderInfo
-    {
-        public int IdentityProviderId { get; set; }
+    public bool? DelegationAccepted { get; set; }
+  }
 
-        public string IdentityProvider { get; set; }
+  public class UserIdentityProviderInfo
+  {
+    public int IdentityProviderId { get; set; }
 
-        public string IdentityProviderDisplayName { get; set; }
-    }
+    public string IdentityProvider { get; set; }
 
-    public class RolePermissionInfo
-    {
-        public int RoleId { get; set; }
+    public string IdentityProviderDisplayName { get; set; }
+  }
 
-        public string RoleName { get; set; }
+  public class RolePermissionInfo
+  {
+    public int RoleId { get; set; }
 
-        public string RoleKey { get; set; }
+    public string RoleName { get; set; }
 
-        public string ServiceClientId { get; set; }
+    public string RoleKey { get; set; }
 
-        public string ServiceClientName { get; set; }
-    }
+    public string ServiceClientId { get; set; }
 
-    public class UserProfileEditRequestInfo : UserDetail
-    {
-        public UserRequestDetail Detail { get; set; }
-    }
+    public string ServiceClientName { get; set; }
+  }
 
-    public class UserProfileResponseInfo : UserDetail
-    {
-        public UserResponseDetail Detail { get; set; }
-    }
+  public class UserProfileEditRequestInfo : UserDetail
+  {
+    public UserRequestDetail Detail { get; set; }
+  }
 
-    public class UserListInfo
-    {
-        public string Name { get; set; }
+  public class UserProfileResponseInfo : UserDetail
+  {
+    public UserResponseDetail Detail { get; set; }
+  }
 
-        public string UserName { get; set; }
+  public class UserListInfo
+  {
+    public string Name { get; set; }
 
-        public int? RemainingDays { get; set; }
+    public string UserName { get; set; }
 
-        public DateTime? StartDate { get; set; }
+    public int? RemainingDays { get; set; }
 
-        public DateTime? EndDate { get; set; }
+    public DateTime? StartDate { get; set; }
 
-        public string? OriginOrganisation { get; set; }
-    }
+    public DateTime? EndDate { get; set; }
 
-    public class AdminUserListInfo
-    {
-        public string FirstName { get; set; }
+    public string? OriginOrganisation { get; set; }
+    
+    public List<RolePermissionInfo> RolePermissionInfo { get; set; }
+  }
 
-        public string LastName { get; set; }
+  public class AdminUserListInfo
+  {
+    public string FirstName { get; set; }
 
-        public string Email { get; set; }
+    public string LastName { get; set; }
 
-        public string Role { get; set; }
-    }
+    public string Email { get; set; }
 
-    public class UserListResponse : PaginationInfo
-    {
-        public string OrganisationId { get; set; }
+    public string Role { get; set; }
+  }
 
-        public List<UserListInfo> UserList { get; set; }
-    }
+  public class UserListResponse : PaginationInfo
+  {
+    public string OrganisationId { get; set; }
 
-    public class AdminUserListResponse : PaginationInfo
-    {
-        public string OrganisationId { get; set; }
+    public List<UserListInfo> UserList { get; set; }
+  }
 
-        public List<AdminUserListInfo> AdminUserList { get; set; }
-    }
+  public class AdminUserListResponse : PaginationInfo
+  {
+    public string OrganisationId { get; set; }
 
-    public class GroupAccessRole
-    {
-        public int GroupId { get; set; }
+    public List<AdminUserListInfo> AdminUserList { get; set; }
+  }
 
-        public string AccessRole { get; set; }
+  public class GroupAccessRole
+  {
+    public int GroupId { get; set; }
 
-        public string AccessRoleName { get; set; }
+    public string AccessRole { get; set; }
 
-        public string Group { get; set; }
+    public string AccessRoleName { get; set; }
 
-        public string ServiceClientId { get; set; }
+    public string Group { get; set; }
 
-        public string ServiceClientName { get; set; }
-    }
+    public string ServiceClientId { get; set; }
 
-    public class UserEditResponseInfo
-    {
-        public string UserId { get; set; }
+    public string ServiceClientName { get; set; }
+  }
 
-        public bool IsRegisteredInIdam { get; set; }
-    }
+  public class UserEditResponseInfo
+  {
+    public string UserId { get; set; }
 
-    public class DelegatedUserProfileRequestInfo
-    {
-        public string UserName { get; set; }
-        public DelegatedUserRequestDetail Detail { get; set; }
-    }
+    public bool IsRegisteredInIdam { get; set; }
+  }
 
-    public class DelegatedUserRequestDetail
-    {
-        public string DelegatedOrgId { get; set; }
+  public class DelegatedUserProfileRequestInfo
+  {
+    public string UserName { get; set; }
+    public DelegatedUserRequestDetail Detail { get; set; }
+  }
 
-        public List<int> RoleIds { get; set; }
+  public class DelegatedUserRequestDetail
+  {
+    public string DelegatedOrgId { get; set; }
 
-        public DateTime StartDate { get; set; }
+    public List<int> RoleIds { get; set; }
 
-        public DateTime EndDate { get; set; }
-    }
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+  }
 }
--- api/CcsSso.Core.Service/External/UserProfileService.cs
+++ api/CcsSso.Core.Service/External/UserProfileService.cs
@@ -268,14 +268,20 @@ namespace CcsSso.Core.Service.External
       // Search for user delegation details for org
       if (isDelegated && !string.IsNullOrWhiteSpace(delegatedOrgId))
       {
-        user = users.SingleOrDefault(u => u.UserType == DbModel.Constants.UserType.Delegation &&
-               u.Party.Person.Organisation.CiiOrganisationId == delegatedOrgId
-               && !u.IsDeleted && u.DelegationEndDate >= DateTime.UtcNow);
+        user = users.SingleOrDefault(u => u.UserType == DbModel.Constants.UserType.Delegation
+               && u.Party.Person.Organisation.CiiOrganisationId == delegatedOrgId
+               && !u.IsDeleted
+               && u.DelegationEndDate >= DateTime.UtcNow);
 
         // If searching for user to delegate in organisation and already exist
         if (isSearchUser && user != default)
         {
           throw new ResourceAlreadyExistsException();
+        }
+        // user delegation not exist
+        else if (user == default)
+        {
+          user = users.SingleOrDefault(u => u.UserType == DbModel.Constants.UserType.Primary);
         }
       }
       // User primary org details
@@ -411,6 +417,8 @@ namespace CcsSso.Core.Service.External
 
       var userPagedInfo = await _dataContext.GetPagedResultAsync(_dataContext.User
         .Include(u => u.Party).ThenInclude(p => p.Person).ThenInclude(o => o.Organisation)
+        .Include(u => u.UserAccessRoles).ThenInclude(gr => gr.OrganisationEligibleRole).ThenInclude(or => or.CcsAccessRole)
+
         // Include deleted for delegated expired
         .Where(u =>
         (isDelegatedExpiredOnly || !u.IsDeleted) && (!isDelegatedExpiredOnly ||  u.DelegationEndDate < DateTime.UtcNow) &&
@@ -440,7 +448,13 @@ namespace CcsSso.Core.Service.External
           StartDate = isDelegatedOnly ? up.DelegationStartDate : default,
           EndDate = isDelegatedOnly ? up.DelegationEndDate : default,
           RemainingDays = !isDelegatedOnly || up.DelegationStartDate is null ? 0 : Convert.ToInt32((up.DelegationEndDate.Value - up.DelegationStartDate.Value).Days),
-          OriginOrganisation = isDelegatedOnly ? up.Party.Person.Organisation.LegalName : default
+          OriginOrganisation = isDelegatedOnly ? up.Party.Person.Organisation.LegalName : default,
+          RolePermissionInfo = !isDelegatedOnly ? default : up.UserAccessRoles.Select(uar => new RolePermissionInfo
+          {
+            RoleId = uar.OrganisationEligibleRole.Id,
+            RoleKey = uar.OrganisationEligibleRole.CcsAccessRole.CcsAccessRoleNameKey,
+            RoleName = uar.OrganisationEligibleRole.CcsAccessRole.CcsAccessRoleName,
+          }).ToList(),
         }).ToList() : new List<UserListInfo>()
       };
 
@@ -1486,9 +1500,10 @@ namespace CcsSso.Core.Service.External
 
       // date validations
       if (userProfileRequestInfo.Detail.StartDate == default || userProfileRequestInfo.Detail.EndDate == default ||
-          userProfileRequestInfo.Detail.StartDate < DateTime.UtcNow ||
-          userProfileRequestInfo.Detail.EndDate > userProfileRequestInfo.Detail.StartDate.AddDays(365) ||
-          userProfileRequestInfo.Detail.StartDate > userProfileRequestInfo.Detail.EndDate)
+          userProfileRequestInfo.Detail.StartDate.Date < DateTime.UtcNow.Date ||
+          userProfileRequestInfo.Detail.EndDate.Date < userProfileRequestInfo.Detail.StartDate.Date.AddDays(28) ||
+          userProfileRequestInfo.Detail.EndDate.Date > userProfileRequestInfo.Detail.StartDate.Date.AddDays(365) ||
+          userProfileRequestInfo.Detail.StartDate.Date > userProfileRequestInfo.Detail.EndDate.Date)
       {
         throw new CcsSsoException(ErrorConstant.ErrorInvalidDetails);
       }
