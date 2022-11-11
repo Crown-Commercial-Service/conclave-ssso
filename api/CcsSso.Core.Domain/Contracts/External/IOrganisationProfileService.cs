9e96cd23c4fefae8241722f9cf5278046ebcc82d
chudasama brijrajsinh <chudasama.brijrajsinh@brickendon.com>
2022-11-11 18:44:48 +0530

    Request context issue for api only resolved, auto validation for supplier type not allowed

--- api/CcsSso.Core.Domain/Contracts/External/IOrganisationProfileService.cs
+++ api/CcsSso.Core.Domain/Contracts/External/IOrganisationProfileService.cs
@@ -35,6 +35,6 @@ namespace CcsSso.Core.Domain.Contracts.External
     
     Task UpdateOrgAutoValidationEligibleRolesAsync(string ciiOrganisationId, RoleEligibleTradeType orgType, List<OrganisationRole> rolesToAdd, List<OrganisationRole> rolesToDelete, string? companyHouseId);
 
-    Task<Tuple<bool, string>> AutoValidateOrganisationDetails(string ciiOrganisationId, string adminEmailId = "", bool verifiedAdminOnly = false);
+    Task<Tuple<bool, string>> AutoValidateOrganisationDetails(string ciiOrganisationId, string adminEmailId = "", bool verifiedAdminOnly = false, bool isReportingMode = false);
   }
 }
