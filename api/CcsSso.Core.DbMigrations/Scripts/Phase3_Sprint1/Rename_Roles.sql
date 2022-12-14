eff6e341ce5b2cefa485a0da7b1b5f9f4d891dd1
brijrajsinh-bc <109584978+brijrajsinh-bc@users.noreply.github.com>
2022-12-14 17:55:41 +0530

    Typo in role rename fixed (#1274)

--- api/CcsSso.Core.DbMigrations/Scripts/Phase3_Sprint1/Rename_Roles.sql
+++ api/CcsSso.Core.DbMigrations/Scripts/Phase3_Sprint1/Rename_Roles.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION Rename_Roles() RETURNS integer AS $$
 BEGIN
 
-	----esorucing service as a supplier
+	----eSourcing service as a supplier
 	UPDATE public."OrganisationAuditEvent" e 
 		SET "Roles" = REPLACE("Roles",r."CcsAccessRoleName",'eSourcing Service as a supplier')
 	FROM public."CcsAccessRole" AS r
@@ -23,7 +23,7 @@ BEGIN
 	FROM public."CcsAccessRole" AS r
 	WHERE r."CcsAccessRoleNameKey" = 'JAEGGER_SUPPLIER' AND e."Roles" like '%,' || r."CcsAccessRoleName" || '';
 
-	----esorucing service as buyer
+	----eSourcing service as buyer
 	UPDATE public."OrganisationAuditEvent" e 
 		SET "Roles" = REPLACE("Roles",r."CcsAccessRoleName",'eSourcing Service as a buyer')
 	FROM public."CcsAccessRole" AS r
@@ -45,25 +45,25 @@ BEGIN
 	FROM public."CcsAccessRole" AS r
 	WHERE r."CcsAccessRoleNameKey" = 'JAEGGER_BUYER' AND e."Roles" like '%,' || r."CcsAccessRoleName" || '';
 
-	----esorucing service - add to dasboard
+	----eSourcing service - add to dashboard
 	UPDATE public."OrganisationAuditEvent" e 
-		SET "Roles" = REPLACE("Roles",r."CcsAccessRoleName",'eSorucing Service - add to dasboard')
+		SET "Roles" = REPLACE("Roles",r."CcsAccessRoleName",'eSourcing Service - add to dashboard')
 	FROM public."CcsAccessRole" AS r
 	WHERE r."CcsAccessRoleNameKey" = 'ACCESS_JAGGAER' AND e."Roles" like '' || r."CcsAccessRoleName" || '';
             
 	UPDATE public."OrganisationAuditEvent" e
-		SET "Roles" = REPLACE("Roles",concat(r."CcsAccessRoleName",','),'eSorucing Service - add to dasboard,')
+		SET "Roles" = REPLACE("Roles",concat(r."CcsAccessRoleName",','),'eSourcing Service - add to dashboard,')
 	FROM public."CcsAccessRole" AS r
 	WHERE r."CcsAccessRoleNameKey" = 'ACCESS_JAGGAER' AND 
 	e."Roles" like '' || r."CcsAccessRoleName" || ',%';
             
 	UPDATE public."OrganisationAuditEvent" e
-			SET "Roles" = REPLACE("Roles", concat(',',r."CcsAccessRoleName",','),',eSorucing Service - add to dasboard,')
+			SET "Roles" = REPLACE("Roles", concat(',',r."CcsAccessRoleName",','),',eSourcing Service - add to dashboard,')
 	FROM public."CcsAccessRole" AS r
 	WHERE r."CcsAccessRoleNameKey" = 'ACCESS_JAGGAER' AND e."Roles" like '%,' || r."CcsAccessRoleName" || ',%';
             
 	UPDATE public."OrganisationAuditEvent" e
-		SET "Roles" = REPLACE("Roles",concat(',',r."CcsAccessRoleName"),',eSorucing Service - add to dasboard')
+		SET "Roles" = REPLACE("Roles",concat(',',r."CcsAccessRoleName"),',eSourcing Service - add to dashboard')
 	FROM public."CcsAccessRole" AS r
 	WHERE r."CcsAccessRoleNameKey" = 'ACCESS_JAGGAER' AND e."Roles" like '%,' || r."CcsAccessRoleName" || '';
 
@@ -113,36 +113,36 @@ BEGIN
 
 	----Contract Award Service (CAS) - add to dashboard
 	UPDATE public."OrganisationAuditEvent" e 
-		SET "Roles" = REPLACE("Roles",r."CcsAccessRoleName",'Contract Award Service (CAS) - add to dasboard')
+		SET "Roles" = REPLACE("Roles",r."CcsAccessRoleName",'Contract Award Service (CAS) - add to dashboard')
 	FROM public."CcsAccessRole" AS r
 	WHERE r."CcsAccessRoleNameKey" = 'ACCESS_CAAAC_CLIENT' AND e."Roles" like '' || r."CcsAccessRoleName" || '';
             
 	UPDATE public."OrganisationAuditEvent" e
-		SET "Roles" = REPLACE("Roles",concat(r."CcsAccessRoleName",','),'Contract Award Service (CAS) - add to dasboard,')
+		SET "Roles" = REPLACE("Roles",concat(r."CcsAccessRoleName",','),'Contract Award Service (CAS) - add to dashboard,')
 	FROM public."CcsAccessRole" AS r
 	WHERE r."CcsAccessRoleNameKey" = 'ACCESS_CAAAC_CLIENT' AND 
 	e."Roles" like '' || r."CcsAccessRoleName" || ',%';
             
 	UPDATE public."OrganisationAuditEvent" e
-			SET "Roles" = REPLACE("Roles", concat(',',r."CcsAccessRoleName",','),',Contract Award Service (CAS) - add to dasboard,')
+			SET "Roles" = REPLACE("Roles", concat(',',r."CcsAccessRoleName",','),',Contract Award Service (CAS) - add to dashboard,')
 	FROM public."CcsAccessRole" AS r
 	WHERE r."CcsAccessRoleNameKey" = 'ACCESS_CAAAC_CLIENT' AND e."Roles" like '%,' || r."CcsAccessRoleName" || ',%';
             
 	UPDATE public."OrganisationAuditEvent" e
-		SET "Roles" = REPLACE("Roles",concat(',',r."CcsAccessRoleName"),',Contract Award Service (CAS) - add to dasboard')
+		SET "Roles" = REPLACE("Roles",concat(',',r."CcsAccessRoleName"),',Contract Award Service (CAS) - add to dashboard')
 	FROM public."CcsAccessRole" AS r
 	WHERE r."CcsAccessRoleNameKey" = 'ACCESS_CAAAC_CLIENT' AND e."Roles" like '%,' || r."CcsAccessRoleName" || '';
 
 ------------Role Name Update-------------
 
-	--esorucing service as a supplier
+	--eSourcing service as a supplier
 	UPDATE public."CcsAccessRole" SET "CcsAccessRoleName" = 'eSourcing Service as a supplier',	"CcsAccessRoleDescription" = 'eSourcing Service as a supplier' WHERE "CcsAccessRoleNameKey" = 'JAEGGER_SUPPLIER';
 	
-	--esorucing service as buyer
+	--eSourcing service as buyer
 	UPDATE public."CcsAccessRole" SET "CcsAccessRoleName" = 'eSourcing Service as a buyer',	"CcsAccessRoleDescription" = 'eSourcing Service as a buyer' WHERE "CcsAccessRoleNameKey" = 'JAEGGER_BUYER';
 	
-	--esorucing service - add to dasboard
-	UPDATE public."CcsAccessRole" SET "CcsAccessRoleName" = 'eSorucing Service - add to dasboard',	"CcsAccessRoleDescription" = 'eSourcing Service - add to dasboard' WHERE "CcsAccessRoleNameKey" = 'ACCESS_JAGGAER';
+	--eSourcing service - add to dashboard
+	UPDATE public."CcsAccessRole" SET "CcsAccessRoleName" = 'eSourcing Service - add to dashboard',	"CcsAccessRoleDescription" = 'eSourcing Service - add to dashboard' WHERE "CcsAccessRoleNameKey" = 'ACCESS_JAGGAER';
 	
 	--eSourcing Service - add service
 	UPDATE public."CcsAccessRole" SET "CcsAccessRoleName" = 'eSourcing Service - add service',	"CcsAccessRoleDescription" = 'eSourcing Service - add service' WHERE "CcsAccessRoleNameKey" = 'JAGGAER_USER';
@@ -151,7 +151,7 @@ BEGIN
 	UPDATE public."CcsAccessRole" SET "CcsAccessRoleName" = 'Contract Award Service (CAS) - add service',	"CcsAccessRoleDescription" = 'Contract Award Service (CAS) - add service' WHERE "CcsAccessRoleNameKey" = 'CAT_USER';
 	
 	--Contract Award Service (CAS) - add to dashboard
-	UPDATE public."CcsAccessRole" SET "CcsAccessRoleName" = 'Contract Award Service (CAS) - add to dasboard',	"CcsAccessRoleDescription" = 'Contract Award Service (CAS) - add to dasboard' WHERE "CcsAccessRoleNameKey" = 'ACCESS_CAAAC_CLIENT';
+	UPDATE public."CcsAccessRole" SET "CcsAccessRoleName" = 'Contract Award Service (CAS) - add to dashboard',	"CcsAccessRoleDescription" = 'Contract Award Service (CAS) - add to dashboard' WHERE "CcsAccessRoleNameKey" = 'ACCESS_CAAAC_CLIENT';
 	
 RETURN 1;
 END;
