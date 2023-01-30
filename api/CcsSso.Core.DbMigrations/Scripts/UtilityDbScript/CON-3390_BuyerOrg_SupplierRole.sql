468e20e3f238bbb09704327f9df988f93fe52ae8
VijayHirudayasamy-bc <vijay.hirudayasamy@brickendon.com>
2023-01-30 15:30:45 +0000

    Script updated to add "IsDeleted" flag

--- api/CcsSso.Core.DbMigrations/Scripts/UtilityDbScript/CON-3390_BuyerOrg_SupplierRole.sql
+++ api/CcsSso.Core.DbMigrations/Scripts/UtilityDbScript/CON-3390_BuyerOrg_SupplierRole.sql
@@ -3,7 +3,7 @@
 select org."Id", org."CiiOrganisationId",org."SupplierBuyerType",org."LegalName", ar."CcsAccessRoleNameKey" 
 from  "OrganisationEligibleRole" oer 
 inner join "CcsAccessRole" ar on ar."Id"=oer."CcsAccessRoleId"
-inner join "Organisation" org on org."Id"=oer."OrganisationId"
+inner join "Organisation" org on org."Id"=oer."OrganisationId" and org."IsDeleted"=false
 where org."SupplierBuyerType"=1 and oer."IsDeleted"=false and ar."CcsAccessRoleNameKey" in ('FP_USER', 'ACCESS_FP_CLIENT')
 order by org."CiiOrganisationId"
 
@@ -15,7 +15,7 @@ UPDATE "Organisation" SET "SupplierBuyerType"=2
 	select distinct org."Id" 
 	from  "OrganisationEligibleRole" oer 
 	inner join "CcsAccessRole" ar on ar."Id"=oer."CcsAccessRoleId"
-	inner join "Organisation" org on org."Id"=oer."OrganisationId"
+	inner join "Organisation" org on org."Id"=oer."OrganisationId" and org."IsDeleted"=false
 	where org."SupplierBuyerType"=1 and oer."IsDeleted"=false and ar."CcsAccessRoleNameKey" in ('FP_USER', 'ACCESS_FP_CLIENT')
 	--and org."Id" in (79,48)
 	 order by org."Id"
--- api/CcsSso.Core.DbMigrations/Scripts/UtilityDbScript/CON-3394-SupplierFleetPortalOrg.sql
+++ api/CcsSso.Core.DbMigrations/Scripts/UtilityDbScript/CON-3394-SupplierFleetPortalOrg.sql
@@ -2,8 +2,8 @@
 --Supplier Org that has the Fleet Portal roles assigned
 select org."CiiOrganisationId",org."LegalName", ar."CcsAccessRoleNameKey" 
 from  "OrganisationEligibleRole" oer 
-inner join "CcsAccessRole" ar on ar."Id"=oer."CcsAccessRoleId"
-inner join "Organisation" org on org."Id"=oer."OrganisationId"
+inner join "CcsAccessRole" ar on ar."Id"=oer."CcsAccessRoleId" 
+inner join "Organisation" org on org."Id"=oer."OrganisationId" and org."IsDeleted"=false
 where org."SupplierBuyerType"=0 and ar."CcsAccessRoleNameKey" in ('FP_USER', 'ACCESS_FP_CLIENT')
 Group by org."CiiOrganisationId",org."LegalName",ar."CcsAccessRoleNameKey"
 
@@ -13,6 +13,6 @@ select org."CiiOrganisationId",org."LegalName", uar."UserId",usr."UserName", ar.
 inner join "OrganisationEligibleRole" oer on oer."Id"=uar."OrganisationEligibleRoleId" 
 inner join "CcsAccessRole" ar on ar."Id"=oer."CcsAccessRoleId"
 inner join "Organisation" org on org."Id"=oer."OrganisationId"
-left join "User" usr on usr."Id"= uar."UserId"
+inner join "User" usr on usr."Id"= uar."UserId" and usr."IsDeleted"=false
 where org."SupplierBuyerType"=0 and ar."CcsAccessRoleNameKey" in ('FP_USER', 'ACCESS_FP_CLIENT')
 Group by org."CiiOrganisationId",org."LegalName",uar."UserId",usr."UserName", ar."CcsAccessRoleNameKey"
