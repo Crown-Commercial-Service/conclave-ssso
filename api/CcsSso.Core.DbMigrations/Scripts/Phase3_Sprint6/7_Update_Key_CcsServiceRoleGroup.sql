7ac552bdfb409fa7143cec61dd0714b026758e3d
VijayHirudayasamy-bc <vijay.hirudayasamy@brickendon.com>
2023-02-24 13:32:36 +0000

    Delegate user changes and database script -for non prod roles and group and fixing few data which were added recently

---
+++ api/CcsSso.Core.DbMigrations/Scripts/Phase3_Sprint6/7_Update_Key_CcsServiceRoleGroup.sql
@@ -0,0 +1,12 @@
+
+
+
+UPDATE "CcsServiceRoleGroup" set "Key"= 'MANAGE_SUBSCRIPTIONS' WHERE "CcsAccessRoleNameKey" = 'MANAGE_SUBSCRIPTIONS_GROUP';
+UPDATE "CcsServiceRoleGroup" set "Key"= 'CAS_USER' WHERE "CcsAccessRoleNameKey" = 'CAS_USER_GROUP';
+UPDATE "CcsServiceRoleGroup" set "Key"= 'FP_USER' WHERE "CcsAccessRoleNameKey" = 'FP_USER_GROUP';
+UPDATE "CcsServiceRoleGroup" set "Key"= 'JAEGGER_BUYER' WHERE "CcsAccessRoleNameKey" = 'JAEGGER_BUYER_GROUP';
+UPDATE "CcsServiceRoleGroup" set "Key"= 'JAEGGER_SUPPLIER' WHERE "CcsAccessRoleNameKey" = 'JAEGGER_SUPPLIER_GROUP';
+UPDATE "CcsServiceRoleGroup" set "Key"= 'ORG_ADMINISTRATOR' WHERE "CcsAccessRoleNameKey" = 'ORG_ADMINISTRATOR_GROUP';
+UPDATE "CcsServiceRoleGroup" set "Key"= 'ORG_DEFAULT_USER' WHERE "CcsAccessRoleNameKey" = 'ORG_DEFAULT_USER_GROUP';
+UPDATE "CcsServiceRoleGroup" set "Key"= 'ORG_USER_SUPPORT' WHERE "CcsAccessRoleNameKey" = 'ORG_USER_SUPPORT_GROUP';
+
