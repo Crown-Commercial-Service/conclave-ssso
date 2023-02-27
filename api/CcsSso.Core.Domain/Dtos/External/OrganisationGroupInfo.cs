d9d0981e82a50be158450f700e006b8e0d9b5acc
Brijesh Patel <Brijesh.Patel@brickendon.com>
2023-02-27 12:24:30 +0530

    Task 4676: Con-2287-Simplify CAS and eSourcing Roles- API CRUD Changes-User/Group

--- api/CcsSso.Core.Domain/Dtos/External/OrganisationGroupInfo.cs
+++ api/CcsSso.Core.Domain/Dtos/External/OrganisationGroupInfo.cs
@@ -70,6 +70,8 @@ namespace CcsSso.Core.Domain.Dtos.External
     public int Id { get; set; }
 
     public string Name { get; set; }
+
+    public string Description { get; set; }
   }
 
   public class GroupUser
