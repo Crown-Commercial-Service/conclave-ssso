dfbd9f38a6a9e899b50feb9dac936779f1e8ad3b
brijrajsinh-bc <109584978+brijrajsinh-bc@users.noreply.github.com>
2023-03-23 20:41:51 +0530

    PPG-211 CAT role description typo fix (#1578)

---
+++ api/CcsSso.Core.DbMigrations/Scripts/Phase3_Sprint7/Update_CAT_Service_Description.sql
@@ -0,0 +1,3 @@
+UPDATE "CcsServiceRoleGroup" SET 
+"Description" = 'Find and contact suitable suppliers for your procurement project, and ask them about the services they can provide. Progress to one stage further competition.'
+WHERE "Key" = 'CAT_USER'
