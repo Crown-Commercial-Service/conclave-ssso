1f03df9206a0a87043b3aded641e8f40a87e9975
brijrajsinh-bc <109584978+brijrajsinh-bc@users.noreply.github.com>
2023-04-07 10:57:38 +0530

    Feature/ppg 212 dev non associated email domain check via managed groups (#1617)
    * PPG-212 - Database Changes (#1582)
    
    * Change to create new request or auto approve it (#1589)
    
    * PPG-212 - Database Changes
    
    * Role Approval (Send fleet role request for approval)
    
    * Group with role approval for manage users (#1594)
    
    * Task 5020: PPG-212-Dev-Non-Associated Email Domain Check via Managed Groups (#1595)
    
    * PPG-212 - Database Changes
    
    * Role Approval (Send fleet role request for approval)
    
    * PPG 212 - Request role approval change
    
    * PPG 212 - Fix merge issue
    
    * PPG 212 - User role approval changes
    
    * PPG 212 - codeclimate fix (#1596)
    
    * PPG-212 - Database Changes
    
    * Role Approval (Send fleet role request for approval)
    
    * PPG 212 - Request role approval change
    
    * PPG 212 - Fix merge issue
    
    * PPG 212 - User role approval changes
    
    * PPG 212 - Code climate fix
    
    * PR-PPG-212-non-associated-email-domain-check-via-managed-groups (#1600)
    
    * Group related changes role approval changes
    
    * Invalid user added/removed case has been handled
    
    * paging has been added
    
    * Minor changes to the property case
    
    * Security api changes to remove pending role approval for group and bugs for manage user solved (#1603)
    
    * Task 5084: PPG 212- Dev -Non-Associated Email Domain Check via Managed Groups (#1605)
    
    * PPG-212 - Database Changes
    
    * Role Approval (Send fleet role request for approval)
    
    * PPG 212 - Request role approval change
    
    * PPG 212 - Fix merge issue
    
    * PPG 212 - User role approval changes
    
    * PPG 212 - Code climate fix
    
    * Task 5084: PPG 212- Dev -Non-Associated Email Domain Check via Managed Groups
    
    * Bug/raj ppg 212 dev non associated email domain check via managed groups (#1607)
    
    * Security api changes to remove pending role approval for group and bugs for manage user solved
    
    * Permission api group role approval improvement
    
    * Bug/raj ppg 212 dev non associated email domain check via managed groups (#1609)
    
    * Security api changes to remove pending role approval for group and bugs for manage user solved
    
    * Permission api group role approval improvement
    
    * Group bug fix for manage my account
    
    * Get user details return group details for expired approval roles (#1616)
    
    * PR-ppg 212 vijay non associated email domain check via managed groups (#1618)
    
    * Group related changes role approval changes
    
    * Invalid user added/removed case has been handled
    
    * paging has been added
    
    * Minor changes to the property case
    
    * Changes done on the IsPending flag which has been replaced with UserPendingRoleStatus Enum
    
    * few issues after buddy check has been resolved
    
    ---------
    
    Co-authored-by: brijeshpatel-bc <108462846+brijeshpatel-bc@users.noreply.github.com>
    Co-authored-by: VijayHirudayasamy-bc <103512567+VijayHirudayasamy-bc@users.noreply.github.com>
    Co-authored-by: Brijesh Patel <Brijesh.Patel@brickendon.com>

--- api/CcsSso.Core.Service/External/OrganisationGroupService.cs
+++ api/CcsSso.Core.Service/External/OrganisationGroupService.cs
@@ -167,15 +167,26 @@ namespace CcsSso.Core.Service.External
         UserId = ugm.User.UserName,
         Name = $"{ugm.User.Party.Person.FirstName} {ugm.User.Party.Person.LastName}",
         IsAdmin = ugm.User.UserAccessRoles.Any(r => !r.IsDeleted && r.OrganisationEligibleRole.CcsAccessRole.CcsAccessRoleNameKey == Contstant.OrgAdminRoleNameKey && !r.OrganisationEligibleRole.IsDeleted),
-        IsPendingApproval = !isApprovalRequired ? false : getUserRolePendingStatus(ugm),
+        
+        UserPendingRoleStaus = !isApprovalRequired ? null : getUserRolePendingStatus(ugm),
       }).ToList();
 
       return organisationGroupResponseInfo;
     }
 
-    private bool getUserRolePendingStatus(UserGroupMembership ugm)
+    private UserPendingRoleStaus? getUserRolePendingStatus(UserGroupMembership ugm)
     {
-      return _dataContext.UserAccessRolePending.OrderByDescending(y => y.Id).FirstOrDefault(x => !x.IsDeleted && x.UserId == ugm.User.Id && x.Status == (int)UserPendingRoleStaus.Pending) != null;
+      // return _dataContext.UserAccessRolePending.OrderByDescending(y => y.Id).FirstOrDefault(x => !x.IsDeleted && x.UserId == ugm.User.Id && x.Status == (int)UserPendingRoleStaus.Pending) != null;
+      var pendingRole = _dataContext.UserAccessRolePending
+          .OrderByDescending(y => y.Id)
+          .FirstOrDefault(x => x.UserId == ugm.User.Id && x.OrganisationUserGroupId == ugm.OrganisationUserGroupId
+            && (x.Status == (int)UserPendingRoleStaus.Pending ||
+                x.Status == (int)UserPendingRoleStaus.Rejected ||
+                x.Status == (int)UserPendingRoleStaus.Removed ||
+                x.Status == (int)UserPendingRoleStaus.Expired));
+
+      
+      return (UserPendingRoleStaus?)(pendingRole?.Status);
     }
 
     public async Task<OrganisationGroupList> GetGroupsAsync(string ciiOrganisationId, string searchString = null)
@@ -450,7 +461,7 @@ namespace CcsSso.Core.Service.External
           await RemoveGroupRolePendingRequest(group, userHasInValidDomain);
 
           List<int> approvalRequiredRoles = new();
-          foreach (var role in group.GroupEligibleRoles)
+          foreach (var role in group.GroupEligibleRoles.Where(x => !x.IsDeleted))
           {
             if (role.OrganisationEligibleRole.CcsAccessRole.ApprovalRequired == (int)RoleApprovalRequiredStatus.ApprovalRequired)
             {
@@ -460,7 +471,14 @@ namespace CcsSso.Core.Service.External
 
           if (approvalRequiredRoles.Any())
           {
-            foreach (var user in userHasInValidDomain)
+            // Check any approved and pending request are there for the user 
+            var existingApprovedUsers = await _dataContext.UserAccessRolePending.Where(x => approvalRequiredRoles.Contains(x.OrganisationEligibleRoleId)
+                && x.OrganisationUserGroupId == group.Id
+                && (x.Status == (int)UserPendingRoleStaus.Approved || x.Status == (int)UserPendingRoleStaus.Rejected || x.Status == (int)UserPendingRoleStaus.Expired)).ToListAsync();
+
+            var usersRequiredPendingRequest = userHasInValidDomain.Where(x => !existingApprovedUsers.Any(y => y.UserId == x.Id));
+
+            foreach (var user in usersRequiredPendingRequest)
             {
               await _userProfileRoleApprovalService.CreateUserRolesPendingForApprovalAsync(new UserProfileEditRequestInfo
               {
@@ -490,9 +508,10 @@ namespace CcsSso.Core.Service.External
 
     private async Task RemoveGroupRolePendingRequest(OrganisationUserGroup group)
     {
-      var pendingGroupRequest = await _dataContext.UserAccessRolePending.Where(x => !x.IsDeleted
-            && x.OrganisationUserGroupId == group.Id
-            && x.Status == (int)UserPendingRoleStaus.Pending).ToListAsync();
+      var pendingGroupRequest = await _dataContext.UserAccessRolePending.Where(x =>
+      (x.Status == (int)UserPendingRoleStaus.Pending ||
+      x.Status == (int)UserPendingRoleStaus.Approved) &&
+      x.OrganisationUserGroupId == group.Id).ToListAsync();
 
       foreach (var pendingRequest in pendingGroupRequest)
       {
@@ -504,10 +523,11 @@ namespace CcsSso.Core.Service.External
 
     private async Task RemoveGroupRolePendingRequest(OrganisationUserGroup group, List<User> users)
     {
-      var pendingGroupRequest = await _dataContext.UserAccessRolePending.Where(x => !x.IsDeleted
-            && x.OrganisationUserGroupId == group.Id
+      var pendingGroupRequest = await _dataContext.UserAccessRolePending.Where(x =>
+            x.OrganisationUserGroupId == group.Id
             && !users.Select(user => user.Id).Contains(x.UserId)
-            && x.Status == (int)UserPendingRoleStaus.Pending).ToListAsync();
+            && (x.Status == (int)UserPendingRoleStaus.Pending
+            || x.Status == (int)UserPendingRoleStaus.Approved)).ToListAsync();
 
       foreach (var pendingRequest in pendingGroupRequest)
       {
@@ -531,10 +551,11 @@ namespace CcsSso.Core.Service.External
       var existingUserIds = group.UserGroupMemberships.Where(x => !x.IsDeleted).Select(ugm => ugm.UserId);
 
       var pendingRequests = await _dataContext.UserAccessRolePending
-          .Where(x => !x.IsDeleted && existingUserIds.Contains(x.UserId) && x.Status == (int)UserPendingRoleStaus.Pending)
+          .Where(x => !x.IsDeleted && existingUserIds.Contains(x.UserId) && x.OrganisationUserGroupId == groupId
+          && (x.Status == (int)UserPendingRoleStaus.Pending || x.Status == (int)UserPendingRoleStaus.Rejected))
           .ToListAsync();
 
-      var filteredUserIds = isPendingApproval ? existingUserIds.Where(x => !pendingRequests.Any(y => y.UserId == x)) : existingUserIds.Where(x => pendingRequests.Any(y => y.UserId == x));
+      var filteredUserIds = isPendingApproval ? existingUserIds.Where(x => pendingRequests.Any(y => y.UserId == x)) : existingUserIds.Where(x => !pendingRequests.Any(y => y.UserId == x));
 
       var usersQuery = _dataContext.User.Include(u => u.Party).ThenInclude(p => p.Person).Where(user => !user.IsDeleted && filteredUserIds.Contains(user.Id)).OrderBy(u => u.UserName);
 
@@ -549,7 +570,7 @@ namespace CcsSso.Core.Service.External
         GroupUser = pagedResult.Results?.Select(up => new GroupUser
         {
           UserId = up.UserName,
-          IsPendingApproval = isPendingApproval,
+          UserPendingRoleStaus = isPendingApproval? UserPendingRoleStaus.Pending: null, // pending and rejected will be shown as users doesn't have the role. 
           Name = $"{up.Party.Person.FirstName} {up.Party.Person.LastName}",
         }).ToList() ?? new List<GroupUser>()
       };
