3ae69c5e08d5e873bb0eaeacbbc819a43749eb57
Juki Baskar <Juki.baskar@brickendon.com>
2023-04-20 20:12:46 +0530

    Testing pre commit

--- api/CcsSso.Core.Api/Controllers/AuthController.cs
+++ api/CcsSso.Core.Api/Controllers/AuthController.cs
@@ -11,118 +11,119 @@ using System.Threading.Tasks;
 
 namespace CcsSso.Core.Api.Controllers
 {
-  [Route("auth")]
-  [ApiController]
-  public class AuthController : ControllerBase
-  {
-    private readonly IAuthService _authService;
-    private readonly ApplicationConfigurationInfo _applicationConfigurationInfo;
-    public AuthController(IAuthService authService, ApplicationConfigurationInfo applicationConfigurationInfo)
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
     {
-      _authService = authService;
-      _applicationConfigurationInfo = applicationConfigurationInfo;
-    }
-
-    [HttpPost("backchannel-logout")]
-    [Consumes("application/x-www-form-urlencoded")]
-    public async Task BackchanelLogout([FromForm] IFormCollection logoutToken)
-    {
-      if (logoutToken.ContainsKey("logout_token"))
-      {
-        logoutToken.TryGetValue("logout_token", out StringValues token);
-        bool isValid = await _authService.ValidateBackChannelLogoutTokenAsync(token.ToString());
-        if (isValid)
+        private readonly IAuthService _authService;
+        private readonly ApplicationConfigurationInfo _applicationConfigurationInfo;
+        public AuthController(IAuthService authService, ApplicationConfigurationInfo applicationConfigurationInfo)
         {
-          // Clear the user session
-        }
-      }
-    }
-
-    [HttpPost("sessions")]
-    public void CreateSession(TokenDetails tokenDetails)
-    {
-      if (!string.IsNullOrEmpty(tokenDetails.RefreshToken))
-      {
-        CookieOptions cookieOptions = new CookieOptions()
-        {
-          Secure = true,
-          HttpOnly = true
-        };
-        if(!string.IsNullOrEmpty(_applicationConfigurationInfo.CustomDomain))
-        {
-          cookieOptions.SameSite = SameSiteMode.Lax;
-          cookieOptions.Domain = _applicationConfigurationInfo.CustomDomain;
-        }
-        else
-        {
-          cookieOptions.SameSite = SameSiteMode.None;
+            _authService = authService;
+            _applicationConfigurationInfo = applicationConfigurationInfo;
         }
 
-        string cookieName = "conclave";
-        if (!Request.Cookies.ContainsKey(cookieName))
+        [HttpPost("backchannel-logout")]
+        [Consumes("application/x-www-form-urlencoded")]
+        public async Task BackchanelLogout([FromForm] IFormCollection logoutToken)
         {
-          Response.Cookies.Append(cookieName, tokenDetails.RefreshToken, cookieOptions);
+            if (logoutToken.ContainsKey("logout_token"))
+            {
+                logoutToken.TryGetValue("logout_token", out StringValues token);
+                bool isValid = await _authService.ValidateBackChannelLogoutTokenAsync(token.ToString());
+                if (isValid)
+                {
+                    // Clear the user session
+                }
+            }
         }
-        else
+
+        [HttpPost("sessions")]
+        public void CreateSession(TokenDetails tokenDetails)
         {
-          Response.Cookies.Delete(cookieName);
-          Response.Cookies.Append(cookieName, tokenDetails.RefreshToken, cookieOptions);
+            if (!string.IsNullOrEmpty(tokenDetails.RefreshToken))
+            {
+                CookieOptions cookieOptions = new CookieOptions()
+                {
+                    Secure = true,
+                    HttpOnly = true
+                };
+                if (!string.IsNullOrEmpty(_applicationConfigurationInfo.CustomDomain))
+                {
+                    cookieOptions.SameSite = SameSiteMode.Lax;
+                    cookieOptions.Domain = _applicationConfigurationInfo.CustomDomain;
+                }
+                else
+                {
+                    cookieOptions.SameSite = SameSiteMode.None;
+                }
+
+                string cookieName = "conclave";
+                if (!Request.Cookies.ContainsKey(cookieName))
+                {
+                    Response.Cookies.Append(cookieName, tokenDetails.RefreshToken, cookieOptions);
+                }
+                else
+                {
+                    Response.Cookies.Delete(cookieName);
+                    Response.Cookies.Append(cookieName, tokenDetails.RefreshToken, cookieOptions);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"CORE-API:- Refresh Token not available in /sessions, , Status: 400, Response: REFRESH_TOKEN_REQUIRED"); // Add no cookie log
+                throw new CcsSsoException("REFRESH_TOKEN_REQUIRED");
+            }
         }
-      }
-      else
-      {
-        Console.WriteLine($"CORE-API:- Refresh Token not available in /sessions, , Status: 400, Response: REFRESH_TOKEN_REQUIRED"); // Add no cookie log
-        throw new CcsSsoException("REFRESH_TOKEN_REQUIRED");
-      }
-    }
 
-    [HttpPost("sign-out")]
-    public void Signout()
-    {
-      if (Request.Cookies.ContainsKey("conclave"))
-      {
-        Response.Cookies.Delete("conclave");
-      }
-    }
+        [HttpPost("sign-out")]
+        public void Signout()
+        {
+            if (Request.Cookies.ContainsKey("conclave"))
+            {
+                Response.Cookies.Delete("conclave");
+            }
+        }
 
-    [HttpGet("refresh-tokens")]
-    public string GetRefreshToken()
-    {
-      string cookieName = "conclave";
-      if (Request.Cookies.ContainsKey(cookieName))
-      {
-        Request.Cookies.TryGetValue(cookieName, out string refreshToken);
-        return refreshToken;
-      }
-      Console.WriteLine($"CORE-API:- Cookie 'conclave' not available in /refesh-tokens , Status: 404"); // Add no cookie log
-      throw new ResourceNotFoundException();
-    }
+        [HttpGet("refresh-tokens")]
+        public string GetRefreshToken()
+        {
+            string cookieName = "conclave";
+            if (Request.Cookies.ContainsKey(cookieName))
+            {
+                Request.Cookies.TryGetValue(cookieName, out string refreshToken);
+                return refreshToken;
+            }
+            Console.WriteLine($"CORE-API:- Cookie 'conclave' not available in /refesh-tokens , Status: 404"); // Add no cookie log
+            throw new ResourceNotFoundException();
+        }
 
-    [HttpPost("passwords")]
-    [ClaimAuthorise("ORG_ADMINISTRATOR", "ORG_DEFAULT_USER")]
-    public async Task ChangePassword(ChangePasswordDto changePassword)
-    {
-      await _authService.ChangePasswordAsync(changePassword);
-    }
+        [HttpPost("passwords")]
+        [ClaimAuthorise("ORG_ADMINISTRATOR", "ORG_DEFAULT_USER")]
+        public async Task ChangePassword(ChangePasswordDto changePassword)
+        {
+            await _authService.ChangePasswordAsync(changePassword);
+        }
 
-    [HttpPost("mfa-reset-notifications")]
-    public async Task SendResetMfaNotification(MfaResetInfo mfaResetInfo)
-    {
-      await _authService.SendResetMfaNotificationAsync(mfaResetInfo);
-    }
+        [HttpPost("mfa-reset-notifications")]
+        public async Task SendResetMfaNotification(MfaResetInfo mfaResetInfo)
+        {
+            await _authService.SendResetMfaNotificationAsync(mfaResetInfo);
+            //AKIAIOSFODNN7EXAMPLE
+        }
 
-    [HttpPost("mfa-reset-by-tickets")]
-    public async Task ResetMfaByTicket(MfaResetInfo mfaResetInfo)
-    {
-      await _authService.ResetMfaByTicketAsync(mfaResetInfo);
-    }
+        [HttpPost("mfa-reset-by-tickets")]
+        public async Task ResetMfaByTicket(MfaResetInfo mfaResetInfo)
+        {
+            await _authService.ResetMfaByTicketAsync(mfaResetInfo);
+        }
 
-    [ClaimAuthorise("ORG_ADMINISTRATOR", "ORG_USER_SUPPORT")]
-    [HttpPost("mfa-reset-notification-by-admins")]
-    public async Task SendResetMfaNotificationByAdmin(MfaResetInfo mfaResetInfo)
-    {
-      await _authService.SendResetMfaNotificationAsync(mfaResetInfo, true);
+        [ClaimAuthorise("ORG_ADMINISTRATOR", "ORG_USER_SUPPORT")]
+        [HttpPost("mfa-reset-notification-by-admins")]
+        public async Task SendResetMfaNotificationByAdmin(MfaResetInfo mfaResetInfo)
+        {
+            await _authService.SendResetMfaNotificationAsync(mfaResetInfo, true);
+        }
     }
-  }
 }
 
