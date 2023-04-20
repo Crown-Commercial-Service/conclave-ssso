624d33dfe04af7d4edd64c7a3f215b4abd2db3b3
brijrajsinh-bc <109584978+brijrajsinh-bc@users.noreply.github.com>
2023-04-20 15:09:05 +0530

    RMI description (#1643)

--- api/CcsSso.Core.DbMigrations/Scripts/Phase3_Sprint7/Update_RMI_Key_Name.sql
+++ api/CcsSso.Core.DbMigrations/Scripts/Phase3_Sprint7/Update_RMI_Key_Name.sql
@@ -1,7 +1,15 @@
 Update "CcsServiceRoleGroup" 
-SET "Name" = 'Report Management Information', "Key" = 'RMI_USER' 
+SET "Name" = 'Report Management Information', "Key" = 'RMI_USER', "LastUpdatedOnUtc" = now()
 WHERE "Key" = 'RMI' AND "IsDeleted" = false;
 
 UPDATE "CcsService" 
-SET "ServiceName" = 'Report Management Information' 
+SET "ServiceName" = 'Report Management Information', "LastUpdatedOnUtc" = now()
+WHERE "ServiceCode" = 'RMI_USER_DS' AND "IsDeleted" = false;
+
+Update "CcsServiceRoleGroup" 
+SET "Description" = 'Use this service to obtain agreement templates, submit management information to CCS or report no business to CCS.', "LastUpdatedOnUtc" = now()
+WHERE "Key" = 'RMI_USER' AND "IsDeleted" = false;
+
+UPDATE "CcsService" 
+SET "Description" = 'Use this service to obtain agreement templates, submit management information to CCS or report no business to CCS.', "LastUpdatedOnUtc" = now()
 WHERE "ServiceCode" = 'RMI_USER_DS' AND "IsDeleted" = false;
