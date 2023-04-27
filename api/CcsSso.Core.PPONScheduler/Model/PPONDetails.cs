c1db84b22a680b44bed691c7113ba97cc38fce95
brijeshpatel-bc <108462846+brijeshpatel-bc@users.noreply.github.com>
2023-04-27 17:36:44 +0530

    PPON Job changes for s8 (#1655)

---
+++ api/CcsSso.Core.PPONScheduler/Model/PPONDetails.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CcsSso.Core.PPONScheduler.Model
+{
+  public class Identifier
+  {
+    public string Id { get; set; }
+
+    [JsonProperty("id-type")]
+    public string IdType { get; set; }
+
+    public bool Persisted { get; set; }
+  }
+
+  public class PPONDetails
+  {
+    public List<Identifier> Identifiers { get; set; }
+  }  
+}
