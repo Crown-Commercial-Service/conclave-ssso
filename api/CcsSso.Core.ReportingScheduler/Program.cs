a1c466147ade589de618a9601d47e99d76cba9e4
brijeshpatel-bc <108462846+brijeshpatel-bc@users.noreply.github.com>
2023-11-16 19:55:16 +0530

    Update Program.cs - For testing

--- api/CcsSso.Core.ReportingScheduler/Program.cs
+++ api/CcsSso.Core.ReportingScheduler/Program.cs
@@ -95,9 +95,9 @@ namespace CcsSso.Core.ReportingScheduler
 
             services.AddSingleton<ICSVConverter, CSVConverter>();
             services.AddSingleton<IFileUploadToCloud, FileUploadToCloud>();
-            services.AddHostedService<OrganisationReportingJob>();
-            services.AddHostedService<UserReportingJob>();
-            services.AddHostedService<ContactReportingJob>();
+            //services.AddHostedService<OrganisationReportingJob>();
+            //services.AddHostedService<UserReportingJob>();
+            //services.AddHostedService<ContactReportingJob>();
             services.AddHostedService<AuditReportingJob>();
           });
     }
