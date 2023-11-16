c9f329052ae38e0bc625d3e8efc396aebee8a35d
brijeshpatel-bc <108462846+brijeshpatel-bc@users.noreply.github.com>
2023-11-16 19:57:11 +0530

    Update AuditReportingJob.cs - For testing

--- api/CcsSso.Core.ReportingScheduler/Jobs/AuditReportingJob.cs
+++ api/CcsSso.Core.ReportingScheduler/Jobs/AuditReportingJob.cs
@@ -57,6 +57,8 @@ namespace CcsSso.Core.ReportingScheduler.Jobs
     {
       try
       {
+        _logger.LogInformation("Audit Reporting Log - Testing");
+        
         var totalNumberOfItemsDuringThisSchedule = 0;
 
         var auditLogResponse = await GetModifiedAuditLog();
